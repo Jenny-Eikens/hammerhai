49ccb9af5050886f22cebc34dab60f7c3a2ab529
Jenny-Eikens <jennifereikens@web.de>
2025-05-15 18:20:05 +0200

    Finish header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const [activeSection, setActiveSection] = useState<string>('')
 
   return (
     <>
@@ -20,10 +21,17 @@ const Header = () => {
             {/* insert correct svg */}
           </button>
           {/* Logo */}
-          <span
-            className="ml-1 pb-1 md:mr-4 md:ml-0 md:items-start"
+          <div
+            className="m-4 bg-white p-4 shadow-lg md:items-start"
             aria-label="Band logo"
-          ></span>
+          >
+            <img
+              src="assets/images/Hammerhai-Logo-Neu.gif"
+              alt="Band logo"
+              className="mb-2"
+            />
+            <p className="text-center text-lg">Punkrock & Ska seit 1997</p>
+          </div>
         </span>
 
         {/* Overlay */}
@@ -36,16 +44,17 @@ const Header = () => {
         {/* Nav bar */}
         {/* slides in from left on small screens, fixed with no transition from medium-sized screens */}
         <nav
-          className={`bg-navbar fixed top-0 left-0 z-40 flex h-full w-[65vw] max-w-[300px] flex-col border border-blue-500 transition-transform duration-300 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:top-auto md:left-auto md:w-auto md:max-w-full md:translate-x-0 md:flex-row md:justify-end md:px-4`}
+          className={`bg-navbar fixed top-0 left-0 z-40 flex h-full w-[65vw] max-w-[300px] flex-col border border-blue-500 transition-transform duration-300 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:top-auto md:left-auto md:h-[70%] md:w-auto md:max-w-full md:translate-x-0 md:flex-row md:justify-end md:px-4`}
         >
           {/* column layout on small screens, row from medium-size */}
-          <ul className="mt-[5rem] list-none space-y-6 border border-red-500 px-4 md:mt-0 md:flex md:flex-row md:items-stretch md:space-y-0 md:space-x-6 md:px-0 md:py-4">
-            {navItems.map((item) => (
+          <ul className="mt-[5rem] list-none space-y-6 border border-red-500 px-4 md:mt-0 md:flex md:flex-row md:items-center md:space-y-0 md:space-x-6 md:px-0 md:py-4">
+            {navItems.map((item, index) => (
               <li
-                key={item}
-                className="bg-white px-6 py-4 shadow-lg md:flex md:h-full md:items-center md:justify-center md:text-center"
+                key={index}
+                className={`overflow-hidden bg-white px-6 py-4 shadow-lg transition-colors md:flex md:h-[75%] md:items-center md:justify-center md:text-center ${activeSection === item.target && 'md:border-underline text-underline md:border-b-6 md:text-inherit'}`}
+                onClick={() => setActiveSection(item.target)}
               >
-                <a href="#">{item}</a>
+                <a href={`#${item.target}`}>{item.title}</a>
               </li>
             ))}
           </ul>
@@ -56,11 +65,26 @@ const Header = () => {
 }
 
 const navItems = [
-  'Start',
-  'Live-Termine',
-  'Merch',
-  'Über uns',
-  'Booking / Kontakt',
+  {
+    target: 'home',
+    title: 'Start',
+  },
+  {
+    target: 'live',
+    title: 'Live-Termine',
+  },
+  {
+    target: 'merch',
+    title: 'Merch',
+  },
+  {
+    target: 'about',
+    title: 'Über uns',
+  },
+  {
+    target: 'contact',
+    title: 'Booking / Kontakt',
+  },
 ]
 
 export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Header from './components/Header'
 const Home = () => {
   return (
     <>
-      <div className="m-auto w-full max-w-[1100px]">
-        <header className="fixed h-[4rem] w-full max-w-[1100px] border border-green-500 bg-transparent px-2 md:h-[6rem] md:px-0">
+      <div className="m-auto w-full max-w-[1400px]">
+        <header className="fixed h-[4rem] w-full max-w-[1400px] border border-green-500 bg-transparent px-2 md:h-[10rem] md:px-0">
           <Header />
         </header>
 
@@ -12,8 +12,15 @@ const Home = () => {
           {/* MAIN CONTENT */}
           <section
             id="home"
-            className="bg-section-gray min-h-screen pt-[6.5rem]"
-          ></section>
+            className="bg-section-gray flex min-h-screen flex-col pt-[10.5rem]"
+          >
+            <div className="mt-auto pb-4">
+              <button className="text-underline mx-auto flex h-[4rem] w-[4rem] items-center justify-center rounded-full bg-white text-4xl">
+                {' '}
+                &darr;
+              </button>
+            </div>
+          </section>
           <section id="live"></section>
           <section id="merch" className="bg-section-gray"></section>
           <section id="about"></section>

e9a32d645e5438f20739051ed7d3481f54a7fe96
Jenny-Eikens <jennifereikens@web.de>
2025-05-26 18:38:50 +0200

    Make footer responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,63 +4,65 @@ import Logo from './Logo/Logo'
 const Footer = () => {
   return (
     <>
-      <div className="flex flex-row border border-violet-600 px-4 py-6">
-        <div className="origin-top-left -translate-y-[3.5%] scale-75 transform border border-amber-500">
-          <Logo />
+      <div className="px-4 py-6">
+        <div className="mb-4 flex flex-col items-center sm:items-start md:flex-row">
+          <div className="scale-75 transform sm:-translate-x-[14%] sm:-translate-y-[15%] md:origin-top-left md:-translate-x-[2.5%] md:-translate-y-[4.5%]">
+            <Logo />
+          </div>
+          <div className="mb-4 flex flex-col flex-wrap space-y-4 gap-y-4 sm:mb-0 sm:flex-row sm:space-y-0 sm:space-x-[2rem] lg:space-x-[4rem]">
+            <div className="footer-details">
+              <h4>BOOKING & KONKTAKT</h4>
+              <address className="not-italic">
+                <div>Christian Sölter</div>
+                <div>Konkordiastraße 6</div>
+                <div>30449 Hannover</div>
+                <a href="mailto:soelti@hammerhai.net">soelti@hammerhai.net</a>
+              </address>
+            </div>
+            <div className="footer-details">
+              <h4>PRESSE</h4>
+              <div>
+                Presse-Paket: <span className="underline">Download</span>
+              </div>
+            </div>
+            <div className="footer-details">
+              <h4>INFORMATION</h4>
+              <div>Datenschutz</div>
+              <div>Disclaimer</div>
+            </div>
+            <div className="footer-details">
+              <h4>IMPRESSUM / WEBMASTER</h4>
+              <address className="not-italic">
+                <div>Jens Berkmann</div>
+                <div>Spichernstr.15</div>
+                <div>42103 Wuppertal</div>
+                <a href="mailto:jb@hammerhai.net">jb@hammerhai.net</a>
+              </address>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-wrap space-x-[2rem] gap-y-4 lg:space-x-[4rem]">
-          <div className="border border-cyan-500">
-            <h4 className="mb-6">BOOKING & KONKTAKT</h4>
-            <address className="not-italic">
-              <div>Christian Sölter</div>
-              <div>Konkordiastraße 6</div>
-              <div>30449 Hannover</div>
-              <a href="mailto:soelti@hammerhai.net">soelti@hammerhai.net</a>
-            </address>
+        <div className="mb-2 flex flex-col-reverse items-center justify-between sm:flex-row sm:space-y-0 sm:space-x-2">
+          <div className="text-sm">
+            Webseite designt und entwickelt von{' '}
+            <a
+              href="https://jennifereikens.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              Jennifer Eikens
+            </a>
+            .{' '}
           </div>
-          <div className="border border-cyan-500">
-            <h4 className="mb-6">PRESSE</h4>
-            <span>
-              Presse-Paket: <span className="underline">Download</span>
-            </span>
+          <div className="mb-8 flex flex-wrap justify-center space-x-5 gap-y-5 sm:mb-0 md:justify-start">
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
+            <div className="h-10 w-10 bg-black"></div>
           </div>
-          <div className="border border-cyan-500">
-            <h4 className="mb-6">INFORMATION</h4>
-            <div>Datenschutz</div>
-            <div>Disclaimer</div>
-          </div>
-          <div className="border border-cyan-500">
-            <h4 className="mb-6">IMPRESSUM / WEBMASTER</h4>
-            <address className="not-italic">
-              <div>Jens Berkmann</div>
-              <div>Spichernstr.15</div>
-              <div>42103 Wuppertal</div>
-              <a href="mailto:jb@hammerhai.net">jb@hammerhai.net</a>
-            </address>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-between gap-y-2 pr-4 pb-2 pl-6">
-        <div className="border border-green-500">
-          Webseite designt und entwickelt von{' '}
-          <a
-            href="https://jennifereikens.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline"
-          >
-            Jennifer Eikens
-          </a>
-          .{' '}
-        </div>
-        <div className="flex flex-wrap space-x-5 gap-y-5 border border-green-500">
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
-          <div className="h-10 w-10 bg-black"></div>
         </div>
       </div>
     </>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -43,9 +43,9 @@ const Header = () => {
 
   return (
     <>
-      <div className="header-wrapper relative flex h-full w-full items-center justify-between pr-1 pl-4 md:px-4">
+      <div className="header-wrapper relative flex h-full w-full items-center justify-between pr-1 pl-4 md:space-x-2 md:px-4">
         {/* Menu toggling button and company logo on left-hand side */}
-        <span className="flex w-full items-center justify-between space-x-2 md:max-w-[400px] md:justify-start">
+        <span className="flex w-full items-center justify-between md:max-w-[300px] md:justify-start md:space-x-2">
           {/* button hidden starting at medium-sized screens */}
           <button
             onClick={() => setMenuOpen(!menuOpen)}
@@ -68,7 +68,7 @@ const Header = () => {
         {/* Nav bar */}
         {/* slides in from left on small screens, fixed with no transition from medium-sized screens */}
         <nav
-          className={`bg-navbar fixed top-0 left-0 z-40 flex h-full w-[75%] max-w-[300px] flex-col overflow-y-auto pb-2 transition-transform duration-300 md:pb-0 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:top-auto md:left-auto md:h-[70%] md:w-auto md:max-w-full md:translate-x-0 md:flex-row md:justify-end md:px-4`}
+          className={`bg-navbar fixed top-0 left-0 z-40 flex h-full w-[75%] max-w-[450px] flex-col overflow-y-auto pb-2 transition-transform duration-300 md:pb-0 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:top-auto md:left-auto md:h-[70%] md:w-auto md:max-w-full md:translate-x-0 md:flex-row md:justify-end md:px-4`}
         >
           {/* column layout on small screens, row from medium-size */}
           <ul className="mt-[8rem] list-none space-y-10 px-4 md:mt-0 md:flex md:flex-row md:flex-wrap md:items-center md:space-y-0 md:space-x-6 md:gap-y-3 md:px-0 md:py-4">
